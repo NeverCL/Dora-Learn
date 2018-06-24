b214137acf8d45f8d9f2f7d9bba24b6c5855aed1
Never、C <caolin@never.cn>
2018-06-24 17:13:13 +0800

    add ef

---
+++ EFCore/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MyDbContext();
+            db.Users.Add(new User{
+                Name = "hello"
+            });
+            db.SaveChanges();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
