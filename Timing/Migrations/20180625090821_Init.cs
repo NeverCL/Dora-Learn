f0db751276b0fe3af1c626184ea450e9960e13e0
neverc <root@neverc.cn>
2018-06-25 17:25:04 +0800

    add TimingDbContext

---
+++ Timing/Migrations/20180625090821_Init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timing.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimerJob",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MethodName = table.Column<string>(maxLength: 128, nullable: true),
+                    Name = table.Column<string>(maxLength: 32, nullable: true),
+                    Period = table.Column<int>(nullable: false),
+                    TypeName = table.Column<string>(maxLength: 128, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimerJob", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimerJob");
+        }
+    }
+}
--- Timing/TimingDbContext.cs
+++ Timing/TimingDbContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Timing
 {
-    public class TimingDbContext
+    public class TimingDbContext:DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=wdora.com;database=TimingDb;uid=root;password=123123;");
+        }
+
+        public DbSet<TimerJob> TimerJob { get; set; }
     }
 }
