c7f5a15bceaf880e9ba7ad74d74c7280fdbf6e07
neverc <root@neverc.cn>
2018-07-05 19:58:20 +0800

    add html engine

--- Learn-React/index.js
+++ Learn-React/index.js
@@ -1,4 +1,16 @@
 var express = require('express');
+var ejs = require('ejs');
+
+
 var app = express();
 app.use(express.static('dist'));
+
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+app.set('views', "dist");
+
+app.use(function (req, res) {
+    res.render('index.html');
+});
+
 app.listen(8080);
