857c60cfff04c0d483149d1dea3ec43cb98c3f6b
neverc <root@neverc.cn>
2018-07-06 11:15:36 +0800

    fix index

--- Learn-React/index.js
+++ Learn-React/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var ejs = require('ejs');
 
-
 var app = express();
+
 app.use(express.static('dist'));
 
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.set('views', "dist");
-
 app.use(function (req, res) {
     res.render('index.html');
 });
