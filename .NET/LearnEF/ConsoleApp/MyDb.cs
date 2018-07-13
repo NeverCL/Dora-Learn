a1ea41334e160ca8fcd5ac096134ecacd925894f
neverc <root@neverc.cn>
2018-07-13 10:58:40 +0800

    add .NET EF

---
+++ .NET/LearnEF/ConsoleApp/MyDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class MyDb : DbContext
+    {
+        public MyDb():base("DefaultConnection")
+        {
+            
+        }
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
---
+++ .NET/LearnEF/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MyDb();
+            db.Menus.Add(new Menu {Name = "hello"});
+            db.SaveChanges();
+        }
+    }
+}
