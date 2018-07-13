b28f69b9307ad4212bb1ff7527a16593baa76f88
Never、C <caolin@never.cn>
2018-07-14 00:02:36 +0800

    add master + slaves

--- .NETCore/Redis/ConsoleApp/Program.cs
+++ .NETCore/Redis/ConsoleApp/Program.cs
@@ -7,16 +7,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var ip = "wdora.com";
+            var serviceName = "mymaster";
+
             ConfigurationOptions sentinelConfig = new ConfigurationOptions();
-            sentinelConfig.ServiceName = "mymaster";
-            sentinelConfig.EndPoints.Add("192.168.0.51", 26379);
-            sentinelConfig.EndPoints.Add("192.168.0.51", 26380);
-            sentinelConfig.EndPoints.Add("192.168.0.51", 26381);
+            sentinelConfig.ServiceName = serviceName;
+            sentinelConfig.EndPoints.Add(ip, 26379);
+            sentinelConfig.EndPoints.Add(ip, 26380);
+            sentinelConfig.EndPoints.Add(ip, 26381);
             sentinelConfig.TieBreaker = "";//这行在sentinel模式必须加上
             sentinelConfig.CommandMap = CommandMap.Sentinel;
             // Need Version 3.0 for the INFO command?
             sentinelConfig.DefaultVersion = new Version(3, 0);
             var conn = ConnectionMultiplexer.Connect(sentinelConfig);
+            // 获取 master 节点信息
+            var master = conn.GetServer(ip,26379).SentinelGetMasterAddressByName(serviceName);
+            var slaves = conn.GetServer(ip,26379).SentinelSlaves(serviceName);
             var sentinelsub= conn.GetSubscriber();
             sentinelsub.Subscribe("+switch-master",(channel, message) =>
             {
