8e42989e27f2df892cd1a51a34daef765ffad6a9
neverc <root@neverc.cn>
2018-07-18 15:25:40 +0800

    add process

---
+++ .NETCore/Process/ConsoleApp/NoxConfig.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp
+{
+    public class NoxConfig
+    {
+        public const string ProcessName = "Process";
+        public static long ProcessId { get; set; }
+        public static string ProcessPath { get; set; }
+    }
+}
---
+++ .NETCore/Process/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (string.IsNullOrEmpty(NoxConfig.ProcessPath))
+            {
+                var process = Process.GetProcessesByName(NoxConfig.ProcessName).FirstOrDefault();
+                if (process == null){
+                    System.Console.WriteLine("等待启动进程....");
+                    Thread.Sleep(3000);
+                }else{
+                    NoxConfig.ProcessId = process.Id;
+                    NoxConfig.ProcessPath = Path.GetDirectoryName(process.MainModule.FileName);
+                }
+            }
+            Console.WriteLine("检测到进程！");
+        }
+    }
+}
